a01af6f11c7872737b4dbd98efb99b3eb2433b05
nokidevz <nokiplays1@gmail.com>
2024-08-27 06:25:49 +0000

    mt6781: Uprev Camera HALs

--- mt6781.mk
+++ mt6781.mk
@@ -94,8 +94,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.device@3.6.vendor:64 \
-    android.hardware.camera.provider@2.6.vendor:64
+    android.hardware.camera.device@3.7.vendor:64 \
+    android.hardware.camera.provider@2.7.vendor:64
 
 PRODUCT_PACKAGES += \
     libcamera_metadata.vendor \
